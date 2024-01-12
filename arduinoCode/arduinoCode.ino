b9007fbd757613415fcc84c3639617e4f9fcc61b
meRybaczek <wojciech.rybaczek@gmail.com>
2024-01-12 20:58:59 +0100

    RFID reader indetification implemented insted of RGB identification

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,87 +1,70 @@
 #include <LiquidCrystal.h>
-
-//LCD
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+#include <SPI.h>
+#include <MFRC522.h>
 
 //LED
 int ON = 1;
 int OFF = 0;
-#define RED_LED 999
-#define GREEN_LED 999
-//define RED AND GREEN pins to do later
-
-//TCS3200 sensor
-#define S0 9
-#define S1 10
-#define S2 11
-#define S3 12
-#define sensorOut 8
-
-//sound sensor
-#define trigPin 9999
-#define echoPin 9999
-
+#define RED_LED A1
+#define GREEN_LED A0
+//RFID
+#define SS_PIN 10
+#define RST_PIN 9
 //buzzer
-#define BUZZER 9999
+#define BUZZER A2
+
+//LCD instance
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+//RFID instance
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 
 void setup() {
-//TCS3200 setup
-  pinMode(S0, OUTPUT);
-  pinMode(S1, OUTPUT);
-  pinMode(S2, OUTPUT);
-  pinMode(S3, OUTPUT);
-  pinMode(sensorOut, INPUT);
-  //TCS output frequency scaling set to 20%
-  digitalWrite(S0, HIGH);
-  digitalWrite(S1, LOW);
-
   //LED
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
+  //buzzer
+  pinMode(BUZZER, OUTPUT);
 
-  //sound sensor
-   pinMode(trigPin, OUTPUT);
-   pinMode(echoPin, INPUT);
 
-   //buzzer
-   pinMode(BUZZER, OUTPUT);
-
-  initialState();
 
   Serial.begin(9600);
   lcd.begin(16, 2);
+
+  initialState();
+
+  SPI.begin();
+  mfrc522.PCD_Init();
 
 
 }
 
 void loop() {
 
-  if(cardInserted()){
-    
+  if(tagDetected()){
+
     beeperOn(1000);
     lcdScreenInfo("Authorizing...");
-    delay(2000); //czas potrzebny na ustabilizowanie karty wstępu w czytniku
-
-    String rgbRead = rgbReaderBegin();
-    sendDataToServer(rgbRead);
+    delay(1000);
+    sendDataToServer();
     String screenText = receiveStringFromServer();
     finalTask(screenText);
     delay(5000);
 
-    lcdScreenInfo("Remove card");
-    while(cardInserted()){
-    }
     initialState(); //powrót do stanu pierwotnego
-   
+
   }
   delay(1000);
 }
 
 void initialState(){
-  lcdScreenInfo("Insert card");
+  lcdScreenInfo("Scan your tag");
   ledsState(RED_LED,ON);
   ledsState(GREEN_LED,OFF);
+}
+
+ boolean tagDetected(){
+    return mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial();
 }
 
 
@@ -93,11 +76,11 @@ void initialState(){
     lcdScreenInfo(message);
   }
 
-  void ledsState(ledName, int state){
+  void ledsState(int ledName, int state){
     if(state == OFF){
-        digitalWrite(ledName, LOW)
+        digitalWrite(ledName, LOW);
     } else{
-        digitalWrite(ledName, HIGH)
+        digitalWrite(ledName, HIGH);
     }
 
   }
@@ -108,71 +91,29 @@ void initialState(){
     lcd.print(message);
   }
 
-  String rgbReaderBegin(){
-    int redReading;
-    int greenReading;
-    int blueReading;
-
-    digitalWrite(S2, LOW);
-    digitalWrite(S3, LOW);
-    redReading = pulseIn(sensorOut, LOW);
-    delay(200);
-
-    digitalWrite(S2, HIGH);
-    digitalWrite(S3, HIGH);
-    greenReading = pulseIn(sensorOut, LOW);
-    delay(200);
-
-    digitalWrite(S2, LOW);
-    digitalWrite(S3, HIGH);
-    blueReading = pulseIn(sensorOut, LOW);
-    delay(200);
-
-    redReading = map(redReading, 343, 172, 0, 255);   //(old_red_reading_for_blackCard,old_red_reading_for_whiteCard,reference_from,reference_to); old_reading is measured for mapping disabled
-    greenReading = map(greenReading, 354, 173, 0, 255);
-    blueReading = map(blueReading, 100, 51, 0, 255);
-
-    return (String(redReading) + "," + String(greenReading) + "," + String(blueReading));
-
-  }
-
-  int countDistanceFromCardToSensor(){
-    int SENSOR_FACTOR = 58;
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-
-    long time = pulseIn(echoPin, HIGH);
-    long distance = time/SENSOR_FACTOR;
-
-    return distance;
-  }
-
   void beeperOn(int beepLength_ms){
     digitalWrite(BUZZER, HIGH);
     delay(beepLength_ms);
-    digitalWrite(BUZZER, HIGH);
+    digitalWrite(BUZZER, LOW);
 
   }
 
-  boolean cardInserted(){
-    int REF_DISTANCE_CM = 10;
-    return countDistanceFromCardToSensor() <= REF_DISTANCE_CM;
-  }
+  void sendDataToServer(){
+        for (byte i = 0; i < mfrc522.uid.size; i++) {
+          Serial.print(mfrc522.uid.uidByte[i], HEX);
+        }
+        delay(500); // Delay to avoid continuous reading
+      }
 
-  void sendDataToServer(String upload){
-    Serial.begin(upload);
 
-  }
   String receiveStringFromServer(){
-    String receivedInfo = "No message received from server";
 
+    String receivedInfo = "No message received from server";
+    delay(2000); // time required to retrieve data from server if exist
     if(Serial.available() > 0){
         receivedInfo = Serial.readStringUntil('\n');
     }
         return receivedInfo;
   }
 
-}
+
